552562541dba0e755ca4f7e5c5f22fdb659a4dd1
Lehnart <thomasserre63@gmail.com>
2025-07-31 16:10:12 +0200

    revert to if-else

--- src/main/java/com/setoh/bgb/tictactoe/ai/BasicEvaluator.java
+++ src/main/java/com/setoh/bgb/tictactoe/ai/BasicEvaluator.java
@@ -14,15 +14,12 @@ public class BasicEvaluator implements Evaluator {
         int currentPlayerCount = 0;
         int opponentCount = 0;
         for (Symbol symbol : line) {
-            switch (symbol) {
-                case EMPTY -> emptyCount++;
-                default -> {
-                    if (symbol == currentPlayer) {
-                        currentPlayerCount++;
-                    } else {
-                        opponentCount++;
-                    }
-                }
+            if (symbol == Symbol.EMPTY) {
+                emptyCount++;
+            } else if (symbol == currentPlayer) {
+                currentPlayerCount++;
+            } else {
+                opponentCount++;
             }
         }
         return new SymbolCount(emptyCount, currentPlayerCount, opponentCount);
