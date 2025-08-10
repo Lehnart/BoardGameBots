37cab1b9562d95e458dd24c994ac1f7b711ca9cc
Lehnart <thomasserre63@gmail.com>
2025-08-10 22:50:51 +0200

    Add the game tree datastructure

---
+++ src/main/java/com/setoh/bgb/tictactoe/GameTree.java
@@ -0,0 +1,39 @@
+package com.setoh.bgb.tictactoe;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GameTree {
+
+    public static class GameTreeNode {
+    }
+
+    private Map<GameTreeNode, List<GameTreeNode>> adjacencyMap = new HashMap<>();
+
+    public void addNode(GameTreeNode node) {
+        if(!adjacencyMap.containsKey(node)) {
+            adjacencyMap.put(node, new ArrayList<>());
+        }
+    }
+
+    public void addEdge(GameTreeNode from, GameTreeNode to) {
+        addNode(from);
+        addNode(to);
+        if (adjacencyMap.containsKey(from)) {
+            adjacencyMap.get(from).add(to);
+        }
+    }
+
+    public List<GameTreeNode> getChildren(GameTreeNode node) {
+        if (adjacencyMap.containsKey(node)) {
+            return adjacencyMap.get(node);
+        }
+        return List.of();
+    }
+
+    public boolean isIn(GameTreeNode node) {
+        return adjacencyMap.containsKey(node);
+    }
+}
---
+++ src/test/java/com/setoh/bgb/tictactoe/GameTreeTest.java
@@ -0,0 +1,39 @@
+package com.setoh.bgb.tictactoe;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameTreeTest {
+
+    @Test
+    public void testAddNode() {
+        GameTree gameTree = new GameTree();
+        GameTree.GameTreeNode node = new GameTree.GameTreeNode();
+
+        assertThat(gameTree.isIn(node)).isFalse();
+        gameTree.addNode(node);
+        assertThat(gameTree.isIn(node)).isTrue();
+    }
+
+    @Test
+    public void testAddEdge() {
+        GameTree gameTree = new GameTree();
+        GameTree.GameTreeNode from = new GameTree.GameTreeNode();
+        GameTree.GameTreeNode to = new GameTree.GameTreeNode();
+        assertThat(gameTree.isIn(from)).isFalse();
+        assertThat(gameTree.isIn(to)).isFalse();
+
+        gameTree.addNode(from);
+        gameTree.addNode(to);
+        
+        assertThat(gameTree.getChildren(from)).isEmpty();
+        assertThat(gameTree.getChildren(to)).isEmpty();
+
+        gameTree.addEdge(from, to);
+
+        assertThat(gameTree.isIn(from)).isTrue();
+        assertThat(gameTree.isIn(to)).isTrue();
+        assertThat(gameTree.getChildren(from)).containsExactly(to);
+    }
+}
